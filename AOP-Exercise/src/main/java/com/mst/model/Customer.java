04a7e8779c9abf0b3d5b82af9538e08fa4a613ab
hajrajaa <129968202+hajrajaa@users.noreply.github.com>
2025-09-28 23:20:19 +0300

    Class Customer added

---
+++ AOP-Exercise/src/main/java/com/mst/model/Customer.java
@@ -0,0 +1,35 @@
+package com.mst.model;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("singelton")
+public class Customer {
+	
+	public void addCustomer()
+	{
+		System.out.println("addCustomer() is running");
+	}
+	
+	public String addCustomerReturnValue()
+	{
+		System.out.println("addCustomerReturnValue()is running");
+		return "Hello MST";
+	}
+	
+	public void addCustomerThrowException () throws Exception
+	{ 
+		System.out.println("addCustomerThrowException() is running "); 
+		throw new Exception ("MST Error"); 
+	
+	}
+	public void addCustomerAround (String name)
+	{ 
+		System.out.println("addCustomerAround () is running, args :" + name); 
+		
+	
+	}
+	
+
+}
